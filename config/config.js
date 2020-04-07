fcd723fc0be1d58c4f7f6b11f4960d1539e00207
Lucio Biancardi <lbiancardi91@gmail.com>
2020-04-07 16:24:05 -0300

    Correciones en config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,3 @@
 process.env.PORT = process.env.PORT || 3000;
 
-
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-
-
-let urlDB = (process.env.NODE_ENV === 'dev') ?
-    'mongodb://localhost:27017/cafe' :
-    process.env.MONGO_URI;
-
-process.env.URLDB = urlDB;
