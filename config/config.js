e0be72553e8d4faa5a140f12cd504c92c4390b78
Lucio Biancardi <lbiancardi91@gmail.com>
2020-04-07 12:25:06 -0300

    Archivo de configuraciones creado.

--- config/config.js
+++ config/config.js
@@ -1,1 +1,12 @@
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3000;
+
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+let urlDB = (process.env.NODE_ENV === 'dev') ?
+    'mongodb://localhost:27017/cafe' :
+    process.env.MONGO_URI;
+
+process.env.URLDB = urlDB;
